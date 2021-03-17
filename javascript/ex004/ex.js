0b1928b582219c66d7056bbe4dcd865b1a9e1a32
elielm14 <elieljuniorm@gmail.com>
2021-03-17 19:52:27 -0300

    comit dos ex de js

---
+++ javascript/ex004/ex.js
@@ -0,0 +1,5 @@
+var vel = 60
+if(vel > 60){
+console.log ('Você foi multado, estavá a cima da velocidade permitida')
+}
+console.log('dirija sempre com cinto de segurança')
---
+++ javascript/ex004/ex1.js
@@ -0,0 +1,6 @@
+var pais = 'EUA'
+if (pais == 'BR'){
+    console.log('Voce é brasileiro')
+}else {
+    console.log('voce é estrangeiro')
+}
