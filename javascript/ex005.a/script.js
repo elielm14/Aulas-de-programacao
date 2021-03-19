c06162d56305117ea1e44c451fe3cae0652386ce
elielm14 <elieljuniorm@gmail.com>
2021-03-19 20:07:50 -0300

    fotos novas

--- javascript/ex005.a/script.js
+++ javascript/ex005.a/script.js
@@ -1,18 +1,16 @@
 function carregar() {
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('img')
+    var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
     msg.innerHTML = `Agora são ${hora} horas.`
 
-    if (hora >= 0 && hora < 12){
-    img.src = 'originalfotomanha.jpg'
-    }
-    else if (hora >= 12 && hora < 18){
-    img.src = 'originalfototarde.jpg'
-    }
-    else {
-    noite
+    if (hora >= 0 && hora < 12) {
+    img.src = 'manha.png'
+    } else if (hora >= 12 && hora < 18) {
+    img.src = 'tarde.png'
+    } else {
+    img.src = 'noite.png'
     }
 
 
